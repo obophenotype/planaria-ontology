8b9516c8194e7c4865c4a01981edd24dc58ddbd9
Sofia Robb <sofia2341@gmail.com>
2019-08-05 14:33:12 -0600

    think i got the edits done

--- src/ontology/run_single_pattern.sh
+++ src/ontology/run_single_pattern.sh
@@ -3,6 +3,7 @@
 #PATTERN=progenitorCellTypes
 #PATTERN=lumenOfAnatomicalEntity
 #PATTERN=primordiumCellTypes
-PATTERN=plana_terms
+#PATTERN=plana_terms
+PATTERN=plana_terms_editsNoParents
 dosdp-tools generate --catalog=catalog-v001.xml --infile=../patterns/data/default/${PATTERN}.tsv --template=../patterns/dosdp-patterns/${PATTERN}.yaml --ontology=plana-edit.owl --obo-prefixes=true --outfile=${PATTERN}.test.owl
 
