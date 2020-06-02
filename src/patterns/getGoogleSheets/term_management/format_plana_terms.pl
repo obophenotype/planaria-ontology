1e97dc2740f85e895fb5c5054b7f40d243b859fb
Sofia Robb <sofia2341@gmail.com>
2020-06-02 12:45:30 -0600

    modified term management script

---
+++ src/patterns/getGoogleSheets/term_management/format_plana_terms.pl
@@ -0,0 +1,151 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+=pod
+=head1 split pattern tsv on object Properties
+
+=item splits the objectProperites that start at column $prop_start into separate TSVs
+
+=item a new pattern will be needed for each new TSV. This pattern only needs the relation, classand var declaration for the objProp value (ie part_of), and the subclass of description.
+
+=item essentially the term gets added first from the plana_terms.yaml and is modified with these objectProp expansion patterns
+ 
+=item pattern.yaml example
+
+pattern_name: plana_terms_part_of.yaml
+pattern_iri: https://tbd
+description: "adding ObjectProperty:part_of to PLANA terms"
+
+contributors:
+  - https://orcid.org/0000-0002-3528-5267
+
+relations:
+  part of: BFO:0000050
+
+classes:
+  part_of: BFO:0000040
+
+vars:
+  part_of: "'part_of'"
+
+subClassOf:
+    text: "'part of' some  %s"
+    vars:
+      - part_of
+
+=cut
+
+# perl ../split_tsv.pl plana_terms.tsv 14
+my $pattern_data_default_dir = '../data/default/';
+my $tsv = shift;
+open TSV, $tsv or die "Cant open $tsv\n";
+
+my $info_line = <TSV>;
+my $header = <TSV>;
+chomp $header;
+my $prop_start;
+my $parent_start;
+my %plana_terms;
+my $term_start;
+
+my @headers = split "\t" , $header;
+for (my $i=0; $i<@headers ; $i++){
+  if ($headers[$i] eq 'part_of'){
+    $prop_start = $i;
+  }elsif($headers[$i] eq 'parent_entity' ){
+    $parent_start = $i;
+  }elsif($headers[$i] eq 'defined_class' ){
+    $term_start = $i;
+  }
+}
+
+my %props;
+my %terms;
+while (my $line = <TSV>){
+  next if $line =~/^\s*$/;
+  chomp $line;
+  my @line = split /\t/, $line;
+  #my ($patterned,$term_id,$term_label) = @line;
+  my ($term_id,$term_label) = @line;
+  #next if $patterned ne 'none';
+  next if $term_label =~ /obsolete/i;
+  next if $term_label =~ /replaced/i;
+  next if $term_label =~ /removed/i;
+  next if !defined $term_id;
+  $term_id =~s/^\s+//;
+  $term_id =~s/\s+$//;
+  $term_label =~s/^\s+//;
+  $term_label =~s/\s+$//;
+  $terms{$term_id}=$term_label;
+
+  my @parent_ids = split /\s*\|\s*/ , $line[$parent_start];
+  my @parent_labels = split /\s*\|\s*/ , $line[$parent_start+1];
+  for (my $i=0; $i<@parent_ids; $i++){
+    for (my $j=0 ; $j<$prop_start ; $j++){
+      if ($j == $parent_start){
+        push @{$plana_terms{$term_id}{$parent_ids[$i]}},$parent_ids[$i];
+      }
+      elsif ($j == $parent_start+1){
+        push @{$plana_terms{$term_id}{$parent_ids[$i]}},$parent_labels[$i];
+
+      }else{
+        if (defined $line[$j]){
+          push @{$plana_terms{$term_id}{$parent_ids[$i]}},$line[$j];
+        }else{
+          #warn "PROBLEMS HERE with uninitialized columns: {$term_id}{$parent_ids[$i]}\n";
+          push @{$plana_terms{$term_id}{$parent_ids[$i]}},'';
+        }
+      }
+    }
+  }
+#print Dumper \%plana_terms;
+  
+  for(my $i=$prop_start; $i < @line; $i+=2){
+    $line[$i] =~ s/^\s+//;
+    $line[$i] =~ s/\s+$//;
+    $line[$i+1] =~ s/^\s+//;
+    $line[$i+1] =~ s/\s+$//;
+    my @ids = split /\s*\|\s*/, $line[$i];
+    my @labels = split /\s*\|\s*/, $line[$i+1];
+    warn "$term_id:$term_label $headers[$i] has unequal pairing of prop ids and labels" if scalar @ids != scalar @labels;
+    for (my $j=0; $j<@ids; $j++){
+      my $id = $ids[$j];
+      my $label = $labels[$j];
+      #$id =~s/^\s+//;
+      #$id =~s/\s+$//;
+      #$label =~s/^\s+//;
+      #$label =~s/\s+$//;
+      $props{$headers[$i]}{$term_id}{$ids[$j]}=$labels[$j];
+    }
+  } 
+} 
+#print Dumper \%props;
+open PLANA_OUT, ">$pattern_data_default_dir/plana_terms.tsv" or die "Can't open $pattern_data_default_dir/plana_terms.tsv for writing\n";
+## print header line
+my @out_headers;
+for (my $i = 0; $i<$prop_start; $i++){
+  push @out_headers, $headers[$i];
+}
+print PLANA_OUT  join("\t",@out_headers),"\n";
+## end print header line
+
+foreach my $term_id (sort keys %plana_terms){ 
+  foreach my $parent_id (sort keys %{$plana_terms{$term_id}}){
+    print PLANA_OUT join ("\t",@{$plana_terms{$term_id}{$parent_id}}),"\n";
+  }
+}
+
+foreach my $prop (sort keys %props){
+  open OUT, ">$pattern_data_default_dir/plana_terms_$prop.tsv" or die "Can't open plana_terms_$prop.tsv for writing\n";
+  print OUT join("\t",$headers[0],$headers[1],$prop,$prop."_label"),"\n";
+  foreach my $term_id(sort keys %{$props{$prop}}){
+    next if !scalar keys %{$props{$prop}{$term_id}};
+    foreach my $prop_id(keys %{$props{$prop}{$term_id}}){
+      print OUT join("\t", $term_id,$terms{$term_id},$prop_id,$props{$prop}{$term_id}{$prop_id}),"\n";
+    }
+  }
+  close OUT;
+}
+#print Dumper \%props;
